52522ed60b9f390b7eb58c0eeb06d999b15172f5
Aniket Gargya <gargya.aniket@gmail.com>
2020-06-02 16:37:21 -0500

    using useEffect instead of useRef

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,10 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export const useAxios = (config: AxiosRequestConfig) => {
-    const initialRequestSent = useRef<boolean>(false);
     const [data, setData] = useState<AxiosResponse<any> | null>(null);
     const [error, setError] = useState<null | any>(null);
-
-    if (!initialRequestSent.current) {
-        initialRequestSent.current = true;
+    useEffect(() => {
         axios(config)
             .then(response => {
                 setData(response);
@@ -15,7 +12,7 @@ export const useAxios = (config: AxiosRequestConfig) => {
             .catch(err => {
                 setError(err);
             });
-    }
+    }, []);
 
     return {
         loading: data ? true : false,
