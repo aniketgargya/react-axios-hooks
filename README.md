46ef10032a5aad9976bd8842bd770abe4732af35
Aniket Gargya <gargya.aniket@gmail.com>
2020-06-02 20:00:58 -0500

    added lazy feature

--- README.md
+++ README.md
@@ -6,14 +6,27 @@ This npm package includes a hook for fetching data with ```axios```. You can use
 
 To import:
 ```
-import { useAxios } from "@aniketgargya/axios-hooks";
+import { useAxios, useLazyAxios } from "@aniketgargya/axios-hooks";
 ```
 
-```
+```javascript
+// Called as soon as component is mounted
 const { response, loading, error } = useAxios({
     url: "https://yourapi.com/path/data.json",
     method: "GET"
 });
+```
+or
+```javascript
+// Called when sendRequest() is called
+const [sendRequest, { response, loading, error }] = useLazyAxios({
+    url: "https://yourapi.com/path/data.json",
+    method: "GET"
+});
+
+handleClick = () => {
+    sendRequest();
+}
 ```
 
 To install:
--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export const useAxios = (config: AxiosRequestConfig) => {
     const [response, setResponse] = useState<AxiosResponse<any> | null>(null);
     const [error, setError] = useState<null | any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
     useEffect(() => {
+        setLoading(true);
         axios(config)
             .then(data => {
+                setLoading(false);
                 setResponse(data);
             })
             .catch(err => {
+                setLoading(false);
                 setError(err);
             });
     }, []);
@@ -19,4 +24,32 @@ export const useAxios = (config: AxiosRequestConfig) => {
         response,
         error
     };
+};
+
+export const useLazyAxios = (config: AxiosRequestConfig): [() => void, { loading: boolean; response: AxiosResponse<any> | null; error: any; }] => {
+    const [response, setResponse] = useState<AxiosResponse<any> | null>(null);
+    const [error, setError] = useState<null | any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const sendRequest = (): void => {
+        setLoading(true);
+        axios(config)
+            .then(data => {
+                setLoading(false);
+                setResponse(data);
+            })
+            .catch(err => {
+                setLoading(false);
+                setError(err);
+            });
+    };
+
+    return [
+        sendRequest,
+        {
+            loading,
+            response,
+            error
+        }
+    ];
 };
